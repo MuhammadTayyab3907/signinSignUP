c24323ec5f8798dd103ae87d9c6a15e332e40000
Tayyab <MuhammadTayyab3907>
2020-11-24 19:17:53 -0800

    Integrate sign in sign up in api

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_login/sharedloginregister.dart';
+import 'package:signinsignupinapi/sharedloginregister.dart';
+
+//void main() => runApp(Login());
+
+void main() => runApp(new MaterialApp(debugShowCheckedModeBanner: false,
+  home: Login(),
+));
